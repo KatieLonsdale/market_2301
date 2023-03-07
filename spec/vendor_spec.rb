f1ec2a065dc825a234f88450cfe4ac8d042086cf
Katie Lonsdale <katie952@gmail.com>
2023-03-07 09:18:56 -0700

    test: add check stock test

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -4,6 +4,8 @@ require './lib/vendor'
 RSpec.describe Vendor do
   before(:each) do
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   describe '#initialize' do
@@ -11,6 +13,12 @@ RSpec.describe Vendor do
       expect(@vendor).to be_a Vendor
       expect(@vendor.name).to eq("Rocky Mountain Fresh")
       expect(@vendor.inventory).to eq({})
+    end
+  end
+
+  describe '#check_stock' do
+    it 'returns number of given items in stock' do
+      expect(@vendor.check_stock(@item1)).to eq 0
     end
   end
 end
