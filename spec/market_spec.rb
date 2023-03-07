adbf1fc1d1f55a89e9466b63cb1674e241578877
Katie Lonsdale <katie952@gmail.com>
2023-03-07 10:06:46 -0700

    test: add sorted item list test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -4,14 +4,14 @@ require './lib/market'
 
 RSpec.describe Market do
   before(:each) do
-    @market = Market.new("South Pearl Street Farmers Market") 
-    @vendor1 = Vendor.new("Rocky Mountain Fresh")
-    @vendor2 = Vendor.new("Ba-Nom-a-Nom")    
-    @vendor3 = Vendor.new("Palisade Peach Shack")    
-    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @market = Market.new('South Pearl Street Farmers Market') 
+    @vendor1 = Vendor.new('Rocky Mountain Fresh')
+    @vendor2 = Vendor.new('Ba-Nom-a-Nom')    
+    @vendor3 = Vendor.new('Palisade Peach Shack')    
+    @item1 = Item.new({name: 'Peach', price: '$0.75'})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+    @item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
     @vendor1.stock(@item1, 35)    
     @vendor1.stock(@item2, 7)    
     @vendor2.stock(@item4, 50)    
@@ -22,7 +22,7 @@ RSpec.describe Market do
   describe '#initialize' do
     it 'exists and has attributes' do
       expect(@market).to be_a Market
-      expect(@market.name).to eq("South Pearl Street Farmers Market")
+      expect(@market.name).to eq('South Pearl Street Farmers Market')
       expect(@market.vendors).to eq([])
     end
   end
@@ -57,4 +57,17 @@ RSpec.describe Market do
       expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
     end
   end
+
+  describe '#sorted_item_list' do
+    it 'returns list of items in stock alphabetically' do
+      @market.add_vendor(@vendor1)    
+      @market.add_vendor(@vendor2)    
+      @market.add_vendor(@vendor3)
+
+      items = ['Banana Nice Cream', 'Peach', 'Peach-Raspberry Nice Cream', 'Tomato']
+
+      expect(@market.sorted_item_list).to eq(items)
+    end
+  end
+
 end
