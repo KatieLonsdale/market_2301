eab2cc7878a17cda51dba307c8485760117415cf
Katie Lonsdale <katie952@gmail.com>
2023-03-07 11:27:44 -0700

    test: format price test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe Item do
       expect(@item2.price).to eq 0.5
     end
   end
+
+  describe '#format_price' do
+    it 'turns string price into float' do
+      expect(@item1.format_price('$0.75')).to eq 0.75
+      expect(@item1.format_price('0.75')).to eq 0.75
+      expect(@item1.format_price('$1')).to eq 1.00
+    end
+  end
 end
