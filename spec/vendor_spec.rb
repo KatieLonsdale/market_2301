4f70648cb20d6fc641c223940f68edbfc8392028
Katie Lonsdale <katie952@gmail.com>
2023-03-07 09:29:39 -0700

    test: test that stock can add another item to inventory

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -23,6 +23,10 @@ RSpec.describe Vendor do
       @vendor.stock(@item1, 30)
       
       expect(@vendor.check_stock(@item1)).to eq 30
+      
+      @vendor.stock(@item1, 25)
+      
+      expect(@vendor.check_stock(@item1)).to eq 55
     end
   end
 
@@ -35,6 +39,9 @@ RSpec.describe Vendor do
       @vendor.stock(@item1, 25)
       
       expect(@vendor.inventory).to eq({@item1 => 55})
+      
+      @vendor.stock(@item2, 12)
+      expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
     end
   end
 end
