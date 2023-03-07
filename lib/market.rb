1849e32cce6ffdfbe5139e4fd3e07fedebd3cb74
Katie Lonsdale <katie952@gmail.com>
2023-03-07 11:00:00 -0700

    feat: add total inventory method

--- lib/market.rb
+++ lib/market.rb
@@ -26,4 +26,21 @@ class Market
     end
     items.flatten.uniq.sort
   end
+
+  def total_inventory
+    total_inventory = Hash.new {|h,k| h[k] = {quantity: 0, vendors: []}}
+    @vendors.map do |vendor| 
+      vendor.inventory.each do |item, quantity|
+        if !total_inventory.key?(item)
+          total_inventory[item]
+          @vendors.each do |vendor|
+            total_inventory[item][:quantity]+= vendor.check_stock(item)
+          end
+        else
+        end
+        total_inventory[item][:vendors] = vendors_that_sell(item)
+      end
+    end
+    total_inventory
+  end
 end
