77ee7a07b574282e5246462694317a18c37f4cad
Katie Lonsdale <katie952@gmail.com>
2023-03-07 09:12:11 -0700

    feat: add initialize method

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,12 @@
 class Item
-  
+  attr_reader :name, :price
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @price = format_price(attributes[:price])
+  end
+
+  def format_price(string_price)
+    string_price.delete('$').to_f
+  end
 end
